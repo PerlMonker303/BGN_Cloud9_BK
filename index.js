9142ac03611d933754e245758ca06454ec68ee24
wavnd <nyonimawande@gmail.com>
2021-10-30 19:36:06 +0200

    added articles, videos and images endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database');
 
+// route for fetching short description of the keyword searched
 app.route('/paragraphs/:keyword')
   .get(function(req, res, next) {
     connection.query(
@@ -16,11 +17,11 @@ app.route('/paragraphs/:keyword')
     );
   });
 
-  // TODO
+// route for fetching youtube playlist link that is of the topic keyword  
 app.route('/videos/:keyword')
   .get(function(req, res, next) {
     connection.query(
-      "SELECT * FROM `videos` WHERE title ILIKE %?% LIMIT 1", req.params.keyword,
+      "SELECT * FROM `videos` WHERE keyword = ? LIMIT 1", req.params.keyword,
       function(error, results, fields) {
         if (error) throw error;
         res.json(results);
@@ -28,6 +29,29 @@ app.route('/videos/:keyword')
     );
   });
 
+// route for fetching articles that contain keyword
+app.route('/articles/:keyword')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `articles` WHERE title LIKE '%"+req.params.keyword+"%';",
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      } 
+    );
+  });
+
+// route for fetching images with the keyword searched
+app.route('/Images/:keyword')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `Images` WHERE keyword = ?", req.params.keyword,
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
 
 app.get('/status', (req, res) => res.send('Working!'));
 
