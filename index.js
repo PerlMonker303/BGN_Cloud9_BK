4b9b668d6254fe59d37c124445e47a04e771394c
Alexandrescu Andrei-Robert <ar_alexandrescu@yahoo.com>
2021-10-30 21:05:55 +0300

    BK tweakings

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config()
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const bodyParser = require('body-parser');
 const connection = require('./database');
 
 // route for fetching short description of the keyword searched
 app.route('/paragraphs/:keyword')
-  .get(function(req, res, next) {
+  .get(function (req, res, next) {
     connection.query(
       "SELECT * FROM `paragraphs` WHERE keyword = ? LIMIT 1", req.params.keyword,
-      function(error, results, fields) {
+      function (error, results, fields) {
         if (error) throw error;
         res.json(results);
       }
@@ -19,10 +21,10 @@ app.route('/paragraphs/:keyword')
 
 // route for fetching youtube playlist link that is of the topic keyword  
 app.route('/videos/:keyword')
-  .get(function(req, res, next) {
+  .get(function (req, res, next) {
     connection.query(
       "SELECT * FROM `videos` WHERE keyword = ? LIMIT 1", req.params.keyword,
-      function(error, results, fields) {
+      function (error, results, fields) {
         if (error) throw error;
         res.json(results);
       }
@@ -31,22 +33,22 @@ app.route('/videos/:keyword')
 
 // route for fetching articles that contain keyword
 app.route('/articles/:keyword')
-  .get(function(req, res, next) {
+  .get(function (req, res, next) {
     connection.query(
-      "SELECT * FROM `articles` WHERE title LIKE '%"+req.params.keyword+"%';",
-      function(error, results, fields) {
+      "SELECT * FROM `articles` WHERE title LIKE '%" + req.params.keyword + "%';",
+      function (error, results, fields) {
         if (error) throw error;
         res.json(results);
-      } 
+      }
     );
   });
 
 // route for fetching images with the keyword searched
-app.route('/Images/:keyword')
-  .get(function(req, res, next) {
+app.route('/images/:keyword')
+  .get(function (req, res, next) {
     connection.query(
       "SELECT * FROM `Images` WHERE keyword = ?", req.params.keyword,
-      function(error, results, fields) {
+      function (error, results, fields) {
         if (error) throw error;
         res.json(results);
       }
@@ -56,5 +58,5 @@ app.route('/Images/:keyword')
 app.get('/status', (req, res) => res.send('Working!'));
 
 // Port 8080 for Google App Engine
-app.set('port', process.env.PORT || 3000);
-app.listen(3000);
+app.set('port', process.env.PORT || 5000);
+app.listen(5000);
