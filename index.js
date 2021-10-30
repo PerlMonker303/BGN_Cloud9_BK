c0e72fcdc962d317bca4abb7658f314d27b2f779
wavnd <nyonimawande@gmail.com>
2021-10-30 15:29:20 +0200

    Initial rest version.
    Rest client that can connect to google cloud instance and database.
    Upon a GET request, can query the paragraphs table and return rows equal to the keyword.
    We probably want to have a `ilike` statement instead of an `=` statement when comparing the keyword column.
    
    We are using mySQL. So connection queries are general sql queries.
    
    TODO would be to create videos and articles tables, along with their routes. Refactor routes into their own
    file, and look into combining the reponse of the json into one big thing that can be sent to front end.
    
    Look into better way of storing environment variables.

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
+
+app.route('/paragraphs/:keyword')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `paragraphs` WHERE keyword = ? LIMIT 1", req.params.keyword,
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
+
+  // TODO
+app.route('/videos/:keyword')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `videos` WHERE title ILIKE %?% LIMIT 1", req.params.keyword,
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
+
+
+app.get('/status', (req, res) => res.send('Working!'));
+
+// Port 8080 for Google App Engine
+app.set('port', process.env.PORT || 3000);
+app.listen(3000);
